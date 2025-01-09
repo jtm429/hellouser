041bc40e4f047bc5d00a8621624d9096a1b785ef
Jacob <jtm429@gmail.com>
2025-01-09 15:16:50 -0600

    Added text input activity

--- app/src/main/java/com/example/hellouser/text_input.kt
+++ app/src/main/java/com/example/hellouser/text_input.kt
@@ -1,31 +1,33 @@
 package com.example.hellouser
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class text_input : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_text_input)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        findViewById<Button>( R.id.hibutton)
+        findViewById<Button>( R.id.chat_button)
             .setOnClickListener {
-                Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
-                val Intent = Intent(this, text_input::class.java)
-                startActivity(Intent)
-        }
-
+                Toast.makeText(this, "Chat", Toast.LENGTH_SHORT).show()
+            }
+        findViewById<Button>( R.id.enter_button)
+            .setOnClickListener {
+                val input = findViewById<EditText>(R.id.editTextText)
+                Toast.makeText(this, "input: ${input.text}", Toast.LENGTH_SHORT).show()
+            }
 
     }
 }
